2067028c3c14dd2d34c82004831e02ec1b1f01e8
timson <timurr.mov@gmail.com>
2024-03-21 17:26:53 +0500

    new_func

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import java.time.OffsetDateTime;
 
 @SpringBootApplication
 @ComponentScan("src/main/java/com/example/myapp")
@@ -14,14 +16,36 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
     @PostMapping("/test")
     public String updateAge(@RequestBody String json) {
 
         JSONObject jsonObject = new JSONObject(json); // Парсим JSON строку
+        JSONObject addValue = jsonObject.getJSONObject("add");
 
-        jsonObject.put("age", 100); // Изменяем значение под ключем "age" на 100
+        // Добавляем новый элемент в массив "alerts" для соответствующего "ticker"
+        JSONArray tickers = jsonObject.getJSONObject("info").getJSONArray("tickers");
 
-        return jsonObject.toString();
+        for (int i = 0; i < tickers.length(); i++) {
+            JSONObject ticker = tickers.getJSONObject(i);
+
+            if (ticker.getString("ticker").equals(addValue.getString("name"))) {
+                JSONArray alerts = ticker.getJSONArray("alerts");
+                JSONObject newAlert = new JSONObject();
+                newAlert.put("timeFrame", addValue.getInt("timeFrame"));
+                newAlert.put("percent", addValue.getInt("percent"));
+                alerts.put(newAlert);
+            }
+        }
+
+        // удаляем поле add
+        jsonObject.remove("add");
+        
+        // обновляем текущее время в ответе
+        String[] currentTime_raw = OffsetDateTime.now().toString().split("\\.");
+        String currentTime = currentTime_raw[0];
+        JSONObject result = jsonObject.put("lastUpdate",currentTime);
+
+
+        return result.toString();
     }
 }
